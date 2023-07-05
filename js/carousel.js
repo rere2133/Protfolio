4e46ba81285ca14086d8460b210a49d10fcb6069
rere2133 <rere2133@users.noreply.github.com>
2023-07-06 02:24:06 +0800

    Add: nav, project

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+  "use strict";
+  if(!$) {
+    return;
+  }
+    // HEADER
+    // $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+
+    // // SMOOTHSCROLL
+    // $(function() {
+    //   $('.nav-link, .custom-btn-link').on('click', function(event) {
+    //     var $anchor = $(this);
+    //     $('html, body').stop().animate({
+    //         scrollTop: $($anchor.attr('href')).offset().top - 49
+    //     }, 1000);
+    //     event.preventDefault();
+    //   });
+    // });  
+
+    // // TOOLTIP
+    // $('.social-links a').tooltip();
+
+})(jQuery);
